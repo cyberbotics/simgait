4d29f4dd350cb8d652c3c4236778afee83248a61
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-01-07 15:47:28 +0100

    Update deploy.php

--- html/deploy.php
+++ html/deploy.php
@@ -1,3 +1,11 @@
 <?php
+  $secret_file = fopen('deploy.secret', 'r') or die("Unable to open deploy.secret file\n");
+  $secret = fread($secret_file, filesize('deploy.secret'));
+  fclose($secret_file);
+  if (!isset($_GET['secret']))
+    die("Missing secret parameter\n");
+  if ($_GET['secret'] !== $secret)
+    die("Wrong secret\n");
   shell_exec('git reset --hard HEAD && git pull');
+  print("OK\n");
 ?>
