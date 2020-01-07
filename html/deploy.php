c70f2e85e11f267fc062b60003fe31f2222a3ba5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-01-07 15:58:53 +0100

    Update deploy.php

--- html/deploy.php
+++ html/deploy.php
@@ -4,8 +4,16 @@
   fclose($secret_file);
   if (!isset($_GET['secret']))
     die("Missing secret parameter\n");
-  if ($_GET['secret'] !== $secret)
-    die("Wrong secret\n");
+  if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE']))
+    die("Missing HTTP_X_HUB_SIGNATURE\n");
+  if (!extension_loaded('hash'))
+    die("Missing 'hash' extension to check the secret code validity\n");
+  list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+  if (!in_array($algo, hash_algos(), TRUE))
+    die("Hash algorithm '$algo' is not supported.");
+  $rawPost = file_get_contents('php://input');
+  if ($hash !== hash_hmac($algo, $rawPost, $secret))
+    die("Hook secret does not match\n");
   shell_exec('git reset --hard HEAD && git pull');
   print("OK\n");
 ?>
