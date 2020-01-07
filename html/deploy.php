4f92a174e10793cb5ad30219c34f2c9fa7f6ed06
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-01-07 15:59:49 +0100

    Update deploy.php

--- html/deploy.php
+++ html/deploy.php
@@ -2,8 +2,6 @@
   $secret_file = fopen('deploy.secret', 'r') or die("Unable to open deploy.secret file\n");
   $secret = fread($secret_file, filesize('deploy.secret'));
   fclose($secret_file);
-  if (!isset($_GET['secret']))
-    die("Missing secret parameter\n");
   if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE']))
     die("Missing HTTP_X_HUB_SIGNATURE\n");
   if (!extension_loaded('hash'))
