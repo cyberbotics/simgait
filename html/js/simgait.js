c97bea2a90ff9241b6abca489ac2c91b7695a950
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-01-22 14:36:07 +0100

    Added sign up modal dialog

---
+++ html/js/simgait.js
@@ -0,0 +1,74 @@
+'uses strict';
+
+class Modal {
+  constructor(selector) {
+    this._element = document.querySelector(selector);
+  }
+  set element(e) {
+    this._element = e;
+  }
+  get element() {
+    return this._element;
+  }
+  show() {
+    Modal.current = this;
+    document.querySelector('html').classList.add('is-clipped');
+    this._element.classList.add('is-active');
+    document.addEventListener('keydown', Modal.escape);
+    this._element.querySelector('button.delete').addEventListener('click', Modal.close);
+    this._element.querySelector('button.cancel').addEventListener('click', Modal.close);
+  }
+  static close(event) {
+    event.preventDefault();
+    Modal.current.hide();
+  }
+  static escape(event) {
+    if (event.keyCode == 27)
+      Modal.close(event);
+  }
+  hide() {
+    this.element.classList.remove('is-active');
+    document.querySelector('html').classList.remove('is-clipped');
+    document.removeEventListener('keydown', Modal.escape);
+    let parent = this.element.parentElement;
+    const clone = this.element.cloneNode(true);  // remove all event listeners
+    parent.removeChild(this.element);
+    parent.appendChild(clone);
+    Modal.current = null;
+  }
+}
+Modal.current = null;
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('a#sign-up').addEventListener('click', function(event) {
+    event.preventDefault();
+    let modal = new Modal('#sign-up-modal');
+    modal.show();
+    modal.element.querySelector('#sign-up-email').focus();
+    let help = modal.element.querySelector("#sign-up-category-help");
+    help.innerHTML = '&nbsp;';
+    modal.element.querySelectorAll('input[name="category"]').forEach((input) => {
+      input.checked = false;
+      input.addEventListener('change', function(event) {
+        const item = event.target.value;
+        switch(item) {
+          case 'developer':
+            help.innerHTML = "Modify simulations: neuromechanical models, environments and locomotion controllers.";
+            break;
+          case 'clinician':
+            help.innerHTML = "Simulate walking gaits of real patients, observe the results of different therapies.";
+            break;
+          case 'educator':
+            help.innerHTML = "Simulate combinations of motor impairments for different pathologies.";
+            break;
+        }
+      });
+    });
+    modal.element.querySelector('form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      modal.hide();
+      console.log("submitted");
+      alert("Not yet functional.")
+    });
+  });
+});
