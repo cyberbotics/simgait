7a68826bd03dac258fbc9a6350ebe81e9790faad
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-01-23 11:55:47 +0100

    Send HTML email

--- html/ajax/sign-up.php
+++ html/ajax/sign-up.php
@@ -2,6 +2,18 @@
   header('Content-Type: application/json');
   $json = file_get_contents('php://input');
   $data = json_decode($json);
-  mail("Olivier.Michel@cyberbotics.com", "SimGait Sign Up", $data->{'email'} . " " . $data->{'category'}, 'From: support@cyberbotics.com');
+  $to = $data->{'email'};
+  $subject = "SimGait sign up";
+  $message = "<html><head><title>$subject</title></head>"
+           . "<body><p>Hello,</p><p>Your request to open a " . $data->{'category'}
+           . " account on <a href=\"https://simgait.org\">simgait.org</a> was sent to the administrator.</p>"
+           . "<p>You will receive a notification as soon as your request is accepted or refused.</p>"
+           . "<p>Best regards,</p><p><a href=\"https://simgait.org\">simgait.org</a></p>\n";
+  $header = "From: support@cyberbotics.com\r\n"
+          . "Reply-To: Olivier.Michel@cyberbotics.com\r\n"
+          . "Cc: Olivier.Michel@cyberbotics.com\r\n"
+          . "MIME-Version: 1.0\r\n"
+          . "Content-type:text/html;charset=UTF-8\r\n";
+  mail($to, $subject, $message, $header);
   echo json_encode($data);
  ?>
