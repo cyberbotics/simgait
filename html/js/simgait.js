e4f35faefc83b0cb41b6361bc2b5c851b2a0983d
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-02-05 12:57:46 +0100

    Cleanup

--- html/js/simgait.js
+++ html/js/simgait.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
            return response.json();
          })
         .then(function(data) {
-           modal.hide();
+           modal.close();
            if (data.error)
              new ModalDialog("Error", data.error);
            else
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let modal = new ModalDialog('Log in', content, 'Cancel', 'Log in');
     modal.querySelector('#log-in-email').focus();
     modal.querySelector('#log-in-forgot').addEventListener('click', function(event) {
-      modal.hide();
+      modal.close();
       content = `
       <div class="field">
         <label class="label">E-mail</label>
@@ -121,7 +121,6 @@ document.addEventListener('DOMContentLoaded', function() {
       </div>
       `;
       let forgot = new ModalDialog('Forgot your password?', content, 'Cancel', 'Reset Password');
-      forgot.show();
       forgot.querySelector('#forgot-email').focus();
       forgot.querySelector('form').addEventListener('submit', function(event) {
         event.preventDefault();
@@ -132,7 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.json();
            })
          .then(function(data) {
-            forgot.hide();
+            forgot.close();
             if (data.error)
               new ModalDialog("Error", data.error);
             else
@@ -184,42 +183,29 @@ class ModalDialog extends HTMLElement {
     `;
     document.querySelector('body').appendChild(this);
   }
-  set title(title) {
-    this.querySelector('p.modal-card-title').innerHTML = title;
-  }
-  set text(text) {
-    this.querySelector('section.modal-card-body').innerHTML = '<div>' + text + '</div>';
-  }
-  set button(button) {
-    this.querySelector('button.cancel').innerHTML = button;
-  }
-  show() {
+  connectedCallback() {
     document.querySelector('html').classList.add('is-clipped');
     this.classList.add('is-active');
     ModalDialog.current = this;
     this.querySelectorAll('p.help').forEach((p) => p.innerHTML = '&nbsp;');
-    document.addEventListener('keydown', function(event) {
-      if (event.keyCode == 27)
-        ModalDialog.close(event);
-    });
+    document.addEventListener('keydown', ModalDialog.closeEvent);
     let submit = this.querySelector('button[type="submit"]');
     if (submit)
       submit.classList.remove('is-loading');
-    this.querySelector('button.delete').addEventListener('click', ModalDialog.close);
-    this.querySelector('button.cancel').addEventListener('click', ModalDialog.close);
-    this.querySelector('.modal-background').addEventListener('click', ModalDialog.close);
+    this.querySelector('button.delete').addEventListener('click', ModalDialog.closeEvent);
+    this.querySelector('button.cancel').addEventListener('click', ModalDialog.closeEvent);
+    this.querySelector('.modal-background').addEventListener('click', ModalDialog.closeEvent);
   }
-  static close(event) {
-    event.preventDefault();
-    ModalDialog.current.hide();
+  static closeEvent(event) {
+    if (event.type == 'click' || (event.type == 'keydown' && event.keyCode == 27)) {
+      event.preventDefault();
+      ModalDialog.current.close();
+    }
   }
-  hide() {
+  close() {
+    document.removeEventListener('keydown', ModalDialog._close);
     ModalDialog.current = null;
-    document.querySelector('body').removeChild(this);
-    return;
-  }
-  connectedCallback() {
-    this.show();
+    this.remove();
   }
 }
 ModalDialog.current = null;
