d94e7fa461a5d7c553748a1d7b34009642a8f63d
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-03-19 09:16:25 +0100

    Fixed structure

--- database/simgait.sql
+++ database/simgait.sql
@@ -1,22 +1,3 @@
-CREATE TABLE `project` (
-  `id` int(11) NOT NULL,
-  `user` int(11) NOT NULL,
-  `repository` varchar(2048) NOT NULL,
-  `branch` varchar(256) NOT NULL,
-  `tag` varchar(256) NOT NULL,
-  `simulator` varchar(256) NOT NULL,
-  `version` varchar(256) NOT NULL,
-  `published` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `request` (
-  `id` int(11) NOT NULL,
-  `user` int(11) DEFAULT NULL,
-  `type` varchar(16) NOT NULL,
-  `token` varchar(32) NOT NULL,
-  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `email` varchar(254) NOT NULL,
@@ -27,24 +8,35 @@ CREATE TABLE `user` (
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`id`),
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `project`
-  ADD KEY `user` (`user`);
-
-ALTER TABLE `request`
-  ADD PRIMARY KEY (`id`),
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `request`
-  ADD KEY `user` (`user`);
-
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `username` (`username`);
+
+CREATE TABLE `request` (
+  `id` int(11) NOT NULL,
+  `user` int(11) DEFAULT NULL,
+  `type` varchar(16) NOT NULL,
+  `token` varchar(32) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id`),
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
+  ADD KEY `user` (`user`);
+
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `user` int(11) NOT NULL,
+  `url` varchar(2048) NOT NULL,
+  `tag` tinyint(1) NOT NULL,
+  `public` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`),
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
+  ADD KEY `user` (`user`);
