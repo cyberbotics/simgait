209266090aaf9b2c75e7f66d27c89bd160c04204
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-09-27 16:15:23 +0200

    charts

--- html/js/animation.js
+++ html/js/animation.js
@@ -157,10 +157,16 @@ export default class Animation extends Project {
     }
 
     function createGraph(index) {
+      let dataPointsIndex = index;
+      if (index === 2)
+        dataPointsIndex = 3;
+      else if (index === 3)
+        dataPointsIndex = 5
+
       const data = {
         labels: labels,
         datasets: [{
-          data: dataPoints[index]
+          data: dataPoints[dataPointsIndex]
         }]
       };
 
