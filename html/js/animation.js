c430a194b610931482a53bbd50b3d9f9887b877a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-12-02 14:24:21 +0100

    Update animation.js

--- html/js/animation.js
+++ html/js/animation.js
@@ -17,7 +17,7 @@ export default class Animation extends Project {
     container.appendChild(view);
     const line = document.createElement('div');
     line.innerHTML = `<a href="https://github.com/cyberbotics/simgait/wiki" target="_blank">How do I create a movie from this animation?</a>`;
-    container.appendChild(line);
+    container.parentNode.appendChild(line);
     view.showCustomWindow = true;
     let basicTimeStep;
     let myCharts;
