33b445aaa632e910dca17e68c382b8e76aa25785
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-24 16:20:27 +0100

    404

--- html/404.php
+++ html/404.php
@@ -1,4 +1,15 @@
 <?php
-http_response_code(404);
+$uri = strtok($_SERVER['REQUEST_URI'], '?');
+if (strlen($uri) == 8 && in_array($uri[1], array('A', 'S')))
+  $found = file_exists('storage' . $uri);
+elseif (in_array($uri, array('/settings', '/animation', '/simulation')))
+  $found = true;
+else
+  $found = false;
+http_response_code($found ? 200 : 404);
+
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
+
 readfile('index.html');
 ?>
