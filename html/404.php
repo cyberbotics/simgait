f451f6bc2740c603cd9b28d0243434680d2dd33d
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-24 16:50:51 +0100

    test

--- html/404.php
+++ html/404.php
@@ -1,7 +1,7 @@
 <?php
 $uri = strtok($_SERVER['REQUEST_URI'], '?');
 if (strlen($uri) == 8 && in_array($uri[1], array('A', 'S')))
-  $found = file_exists('storage' . $uri);
+  $found = true;
 elseif (in_array($uri, array('/settings', '/animation', '/simulation')))
   $found = true;
 else
