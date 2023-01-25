adc85f7675c1c3ad6f9cbaa2f31bfb11edc2a7a9
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-25 09:50:06 +0100

    test

--- html/ajax/animation/list.php
+++ html/ajax/animation/list.php
@@ -24,10 +24,12 @@
     $url = $mysqli->escape_string($data->url);
     $uri = substr($url, strrpos($url, '/'));
     $uploadMessage = "?upload=webots";
-    set_error_handler("myErrorHandler");
-    if (str_ends_with($uri, $uploadMessage))
-      $uri = substr($uri, 0, strrpos($uri, '?'));
-
+    try {
+      if (str_ends_with($uri, $uploadMessage))
+        $uri = substr($uri, 0, strrpos($uri, '?'));
+    } catch (\Throwable $e) {
+        die("Caught exception:");
+    }
     die("skip catch");
     $id = string_to_mysql_id(substr($uri, 2)); // skipping '/A'
     $query = "UPDATE animation SET viewed = viewed + 1 WHERE id=$id";
@@ -88,8 +90,4 @@
   $answer->animations = $animations;
   $answer->total = intval($count['count']);
   die(json_encode($answer));
-
-  function myErrorHandler($errno, $errstr, $errfile, $errline) {
-    die("bonjour");
-  }
  ?>
