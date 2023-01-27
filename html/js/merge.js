7399fab1011d73f69334cd16b0bab8ef3e870906
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-27 17:12:13 +0100

    merge animatioN

--- html/js/merge.js
+++ html/js/merge.js
@@ -1,10 +1,16 @@
-import WebotsView from '../../../webots/resources/web/wwi/WebotsView.js';
-import {getNodeAttribute} from '../../../webots/resources/web/wwi/Parser.js';
+import WebotsView from '../../../webots2/resources/web/wwi/WebotsView.js';
+import {getNodeAttribute} from '../../../webots2/resources/web/wwi/Parser.js';
 
 const container = document.getElementsByClassName('webots-view-container')[0];
 const webotsView = new WebotsView();
 
 container.appendChild(webotsView);
+
+const j1 = fetch('storage/18_Thelen_Ong2019/animation.json')
+  .then(result => result.json());
+
+const j2 = fetch('storage/18_Millard_Ong2019/animation.json')
+  .then(result => result.json());
 
 const p1 = fetch('storage/18_Thelen_Ong2019/model.x3d')
   .then(result => result.text())
@@ -25,16 +31,45 @@ const p2 = fetch('storage/18_Millard_Ong2019/model.x3d')
     const parser = new DOMParser();
     return parser.parseFromString(text, 'text/xml');
   });
-Promise.all([p1, p2]).then(() => {
+
+// TODO make possible to merge 2 in 1 if 2 is smaller
+Promise.all([p1, p2, j1, j2]).then(() => {
   p2.then(p2 => {
     const root = p2.getElementsByTagName('Scene')[0];
-    let maxId = findMaxId(root, -1);
+    const maxId = findMaxId(root, -1);
     p1.then(skeleton => {
+      // Combine x3d
       skeleton.id = 'n' + (parseInt(skeleton.id.substr(1)) + maxId);
       increaseId(skeleton, maxId);
       root.appendChild(skeleton);
-      webotsView.loadAnimation(new XMLSerializer().serializeToString(p2), 'storage/18_Millard_Ong2019/animation.json', 
-        undefined, undefined, undefined, true);
+
+      // Combine json
+      j1.then(json1 => {
+        j2.then(json2 => {
+          const array1 = json1.frames;
+          const array2 = json2.frames;
+          // We assume that all animation have the same timestep.
+          const length = array1.length > array2.length ? array1.length : array2.length;
+
+          // Merge ids
+          let ids1 = json1.ids.split(';');
+          ids1 = ';' + ids1.map(id => parseInt(id) + maxId).join(';');
+          json2.ids += ids1;
+
+          // Merge frames
+          for (let i = 0; i < length; i++) {
+            const updates1 = array1[i].updates;
+            const updates2 = array2[i].updates;
+
+            for (const update of updates1) {
+              update.id += maxId;
+              updates2.push(update);
+            }
+          }
+          webotsView.loadAnimation(new XMLSerializer().serializeToString(p2), json2,
+            undefined, undefined, undefined, true);
+        });
+      });
     });
   });
 });
