9670e16f53ca74763bdd2fc8e9ec45f495ddf6b0
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-02-02 11:01:32 +0100

    test

--- html/js/GraphWindow.js
+++ html/js/GraphWindow.js
@@ -143,6 +143,7 @@ export default class GraphWindow {
 
   createGraphs(json) {
     if (typeof this.view.toolbar !== 'undefined') {
+      console.log(this.view)
       this.view.setCustomWindowTitle('Interactive Charts');
       this.view.setCustomWindowTooltip('Interactive Charts');
       this.view.setCustomWindowContent(`
